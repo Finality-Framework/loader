902767b114d71452272d46bd18c601345d264b9d
RedreamR <crystall1ne@outlook.com>
2024-11-16 18:08:20 +0800

    Better log

--- src/main/java/team/rainfall/finality/FinalityLogger.java
+++ src/main/java/team/rainfall/finality/FinalityLogger.java
@@ -5,6 +5,7 @@ public class FinalityLogger {
     public static final String RED_BACKGROUND = "\033[41m";
     public static final String WHITE_BACKGROUND = "\033[47m";
     public static final String YELLOW_BACKGROUND = "\033[43m";
+    public static final String GRAY_BACKGROUND = "\033[100m";
     public static final String BLACK_COLOR = "\033[30m";
     public static final String RESET = "\033[0m";
     public static void info(String message) {
@@ -21,7 +22,7 @@ public class FinalityLogger {
     }
     public static void debug(String message) {
         if(isDebug) {
-            System.out.println("[Debug] " + message);
+            System.out.println(GRAY_BACKGROUND+"[Debug] " + message+RESET);
         }
     }
     public static void warn(String message) {
