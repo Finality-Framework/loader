7bb558ee2345c90336acaf2e26b9e0036a3d20e9
RedreamR <crystall1ne@outlook.com>
2025-03-15 23:25:52 +0800

    Version Type changed

--- src/main/java/team/rainfall/finality/loader/Main.java
+++ src/main/java/team/rainfall/finality/loader/Main.java
@@ -18,7 +18,7 @@ import team.rainfall.finality.loader.gui.ErrorCode;
 public class Main {
     public static final String CODENAME = "RAIDEN";
     public static final String VERSION = "1.3.0";
-    public static final VersionType VERSION_TYPE = VersionType.DEV;
+    public static final VersionType VERSION_TYPE = VersionType.RELEASE;
     public static final String STEAM_MANAGER_CLASS = "aoh.kingdoms.history.mainGame.Steam.SteamManager";
     public static String LAUNCHER_CLASS = "aoh.kingdoms.history.mainGame.desktop.DesktopLauncher";
     public static ArrayList<String> localMods = new ArrayList<>();
