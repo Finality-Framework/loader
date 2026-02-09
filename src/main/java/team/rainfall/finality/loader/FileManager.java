89aeb9135c50a5b0756aa3322bc79cf5e189180b
RedreamR <crystall1ne@outlook.com>
2026-02-09 11:17:57 +0800

    [refactor] clean code

--- src/main/java/team/rainfall/finality/loader/FileManager.java
+++ src/main/java/team/rainfall/finality/loader/FileManager.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package team.rainfall.finality.loader;
 
 import team.rainfall.finality.FinalityLogger;
